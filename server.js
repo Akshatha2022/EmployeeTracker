891e3e1c148688c0bbdae32b1e88f3a76e7e3696
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-03 07:58:00 -0500

    adding view all roles

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const inquirer = require("inquirer");
 require("console.table");
 const chalk = require("chalk");
 const figlet = require("figlet");
-const validate = require("./javascript/validate");
+//const validate = require("./javascript/validate");
 const mysql = require("mysql2");
-const data = require('./queries');
+//const data = require('./queries');
 
 require("dotenv").config();
 // console.log (process.env.DB_PASSWORD)
@@ -139,3 +139,44 @@ function mainMenu() {
     });
   };
 }
+
+// View Functions
+
+// View All Employees
+const viewAllEmployees = () => {
+  let sql =       `SELECT employee.id, 
+                  employee.firstname, 
+                  employee.lastname, 
+                  role.title, 
+                  department.department_name AS 'department', 
+                  role.salary
+                  FROM employee, role, department 
+                  WHERE department.id = role.department_id 
+                  AND role.id = employee.role_id
+                  ORDER BY employee.id ASC`;
+  connection.promise().query(sql, (error, reply) => {
+    if (error) throw error;
+    console.log(chalk.yellow.bold(`====================================================================================`));
+    console.log(`                              ` + chalk.green.bold(`Current Employees:`));
+    console.log(chalk.yellow.bold(`====================================================================================`));
+    console.table(reply);
+    console.log(chalk.yellow.bold(`====================================================================================`));
+    promptUser();
+  });
+};
+
+// View all Roles
+const viewAllRoles = () => {
+  console.log(chalk.yellow.bold(`====================================================================================`));
+  console.log(`                              ` + chalk.green.bold(`Current Employee Roles:`));
+  console.log(chalk.yellow.bold(`====================================================================================`));
+  const sql =     `SELECT role.id, role.title, department.department_name AS department
+                  FROM role
+                  INNER JOIN department ON role.department_id = department.id`;
+  connection.promise().query(sql, (error, reply) => {
+    if (error) throw error;
+      reply.forEach((role) => {console.log(role.title);});
+      console.log(chalk.yellow.bold(`====================================================================================`));
+      promptUser();
+  });
+};
