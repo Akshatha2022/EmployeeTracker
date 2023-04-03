e73a43f451764e5bbfbf615dd1b4f0e06f369bd3
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-03 12:11:41 -0500

    delete a role

--- server.js
+++ server.js
@@ -554,3 +554,42 @@ const removeEmployee = () => {
       });
   });
 };
+
+// Delete a Role
+const removeRole = () => {
+  let sql = `SELECT role.id, role.title FROM role`;
+
+  connection.promise().query(sql, (error, reply) => {
+    if (error) throw error;
+    let roleNamesArray = [];
+    reply.forEach((role) => {roleNamesArray.push(role.title);});
+
+    inquirer
+      .prompt([
+        {
+          name: 'chosenRole',
+          type: 'list',
+          message: 'Which role would you like to remove?',
+          choices: roleNamesArray
+        }
+      ])
+      .then((answer) => {
+        let roleId;
+
+        response.forEach((role) => {
+          if (answer.chosenRole === role.title) {
+            roleId = role.id;
+          }
+        });
+
+        let sql =   `DELETE FROM role WHERE role.id = ?`;
+        connection.promise().query(sql, [roleId], (error) => {
+          if (error) throw error;
+          console.log(chalk.redBright.bold(`====================================================================================`));
+          console.log(chalk.greenBright(`Role Successfully Removed`));
+          console.log(chalk.redBright.bold(`====================================================================================`));
+          viewAllRoles();
+        });
+      });
+  });
+};
